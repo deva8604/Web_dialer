af2581a09f712a8cb71a16696df34de5e980ab23
Devesh <deveshkumarmm4@gmail.com>
2025-11-25 18:19:23 +0530

    spaces deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Feature from './Components/Feature.jsx';
+import Feature from './Components/Feature.jsx'
 import Callhistory from './Components/Callhistory.jsx'  
 import Dialer from './Components/Dialer.jsx'
 
@@ -12,8 +12,7 @@ function App() {
 
   return (
     <>
-   
-  <Dialer/>
+   <Dialer/>
    <Callhistory/>
     </>
   )
